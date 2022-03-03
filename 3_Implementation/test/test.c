15ef0fd0b1b5837a776136d864e4b9dc5495b669
Keshav Tiwari <56231666+KeshavTiwari23@users.noreply.github.com>
2022-03-04 00:11:28 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,23 @@
 
+#include "unity.h"
+#include "CheckYourGK.h"
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_a(void){
+   char name[20]="Keshav";
+   int number=100;
+   TEST_ASSERT_EQUAL("Keshav",name);
+   TEST_ASSERT_EQUAL(100,number);
+    
+}
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_a);
+  return UNITY_END();
+}
