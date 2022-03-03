3b5ee19dd675a412bcd05b5922ca39cca2fbed32
Keshav Tiwari <56231666+KeshavTiwari23@users.noreply.github.com>
2022-03-04 00:23:01 +0530

    Create CheckYourGK.h

---
+++ 3_Implementation/inc/CheckYourGK.h
@@ -0,0 +1,20 @@
+/**
+ * @file hangman.c
+ * @author Naman Thakur (namanthakur97@gmail.com)
+ * @brief Hangman Game in C
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
